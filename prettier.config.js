102b27efc0a83ede3fcd3b4f4d3e6f4250171c9f
Abdulfatahi <abdulfatahi.ishaq@indiecampers.com>
2023-02-05 22:11:11 +0100

    [chore] Setup project config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  requirePragma: false,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+}
