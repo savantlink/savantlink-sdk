f0ba1fcfa2ad64090e12d2da57f11d9dd03f791a
abfatahi <abfatahi.iaf@gmail.com>
2025-03-09 18:09:35 +0100

    refactor: toast, input component

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,8 @@ Types of changes are:
 6. Security - For security features
 
 ## Unreleased
+### Added
+- Created `Toast` cmponent
 
 ## 1.0.18 - 2025-03-09
 ### Changed
---
+++ custom.d.ts
@@ -0,0 +1,8 @@
+declare module '*.scss';
+
+declare module '*.svg' {
+  import React from 'react';
+  const content: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default content;
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+}
